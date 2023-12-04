dcf4006947e6319b21ff28302d42d80d2bd2f001
Will Hoppin <whoppin@middlebury.edu>
2023-12-04 16:53:27 -0500

    host our data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,42 @@
 import Image from 'next/image'
 import mockData from '../../mockdata.json'; // Importing the mock data
 
+interface Payment {
+  name: string;
+  amount: number;
+}
+
+interface DailyPayment {
+  date: string;
+  payments: Payment[];
+}
+
 interface Movie {
   projectName: string;
   photoExtension: string;
-  genre: string;
+  dailyResidualPayments: DailyPayment[];
 }
 
-
 export default function Home() {
+  // Function to calculate total residuals
+  const calculateTotalResiduals = (dailyResidualPayments: DailyPayment[]) => {
+    return dailyResidualPayments.reduce((total, day) => {
+      return total + day.payments.reduce((dayTotal: number, payment) => dayTotal + payment.amount, 0);
+    }, 0);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-gray-800 bg-white">
       {/* ... other components ... */}
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         {mockData.map((movie: Movie) => (
-          <div key={movie.projectName} className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-            <h2 className="mb-3 text-2xl font-semibold">
+          <div key={movie.projectName} className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 cursor-pointer">
+            <h2 className="mb-3 text-2xl font-semibold" style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>
               {movie.projectName}
             </h2>
             <p className="m-0 max-w-[30ch] text-sm opacity-50">
-              Genre: {movie.genre}
+              Total Residuals: ${calculateTotalResiduals(movie.dailyResidualPayments).toLocaleString()}
             </p>
             <Image
               src={`/images/${movie.photoExtension}.png`} // Updated to .png
