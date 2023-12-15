abbe6c4a1f36cfd47771879a693874887444ceea
Will Hoppin <whoppin@middlebury.edu>
2023-12-15 12:21:00 -0500

    new homepage design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
-import Image from 'next/image'
+'use client';
+import React, { useState } from 'react';
+import Image from 'next/image';
 import mockData from '../../mockdata.json'; // Importing the mock data
 
 interface Payment {
@@ -11,43 +13,189 @@ interface DailyPayment {
   payments: Payment[];
 }
 
+interface CastAndCrewMember {
+  name: string;
+  points: number;
+  walletAddress: string;
+}
+
 interface Movie {
   projectName: string;
   photoExtension: string;
   dailyResidualPayments: DailyPayment[];
+  castAndCrew: CastAndCrewMember[];
 }
 
 export default function Home() {
-  // Function to calculate total residuals
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+  const [showCreateMovieForm, setShowCreateMovieForm] = useState(false);
+  const [newMovie, setNewMovie] = useState<Movie | null>(null);
+
   const calculateTotalResiduals = (dailyResidualPayments: DailyPayment[]) => {
-    return dailyResidualPayments.reduce((total, day) => {
-      return total + day.payments.reduce((dayTotal: number, payment) => dayTotal + payment.amount, 0);
-    }, 0);
+    return dailyResidualPayments.reduce((total, day) => (
+      total + day.payments.reduce((dayTotal: number, payment) => dayTotal + payment.amount, 0)
+    ), 0);
   };
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-gray-800 bg-white">
-      {/* ... other components ... */}
+  const calculateIndividualPayment = (name: string) => {
+    let totalPayment = 0;
+    selectedMovie?.dailyResidualPayments.forEach(day => {
+      day.payments.forEach(payment => {
+        if (payment.name === name) {
+          totalPayment += payment.amount;
+        }
+      });
+    });
+    return totalPayment;
+  };
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+  const handleCreateMovie = (movie: Movie) => {
+    // Here you would send the movie to your backend to be added to the JSON file
+    mockData.push(movie); // Adding to the existing data for demo purposes
+    setShowCreateMovieForm(false);
+  };
+
+  const handleNewMovieChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewMovie({
+      ...newMovie,
+      [e.target.name]: e.target.value
+    } as Movie);
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (newMovie) {
+      handleCreateMovie(newMovie);
+      setNewMovie(null);
+    }
+  };
+
+  if (showCreateMovieForm) {
+    return (
+      <div className="bg-white text-black">
+        <h2>New Movie</h2>
+        <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column' }}>
+          <input type="text" name="projectName" placeholder="Project Name" onChange={(event) => handleNewMovieChange(event)} required />
+          <input type="text" name="photoExtension" placeholder="Photo Extension" onChange={(event) => handleNewMovieChange(event)} required />
+
+          {/* Additional Fields */}
+          <select name="typeOfProject" onChange={(event) => handleNewMovieChange(event)} required>
+            {/* Options for type of project */}
+          </select>
+          <select name="genre" onChange={(event) => handleNewMovieChange(event)} required>
+            {/* Options for genre */}
+          </select>
+          <div>
+            {/* Distribution methods as checkboxes */}
+          </div>
+          <select name="primaryDistributionMethod" onChange={(event) => handleNewMovieChange(event)} required>
+            {/* Options for primary distribution method */}
+          </select>
+          <input type="number" name="budget" placeholder="Budget" onChange={(event) => handleNewMovieChange(event)} required />
+
+          {/* Dynamic Fields for Cast */}
+          <div id="castContainer" style={{ display: 'flex', flexDirection: 'column' }}>
+            {/* Fields for each cast member */}
+          </div>
+          <button type="button" onClick={addCastMember}>Add Cast Member</button>
+
+          {/* Dynamic Fields for Crew */}
+          <div id="crewContainer" style={{ display: 'flex', flexDirection: 'column' }}>
+            {/* Fields for each crew member */}
+          </div>
+          <button type="button" onClick={addCrewMember}>Add Crew Member</button>
+
+          <button type="submit">Create Movie</button>
+        </form>
+        <button onClick={() => setShowCreateMovieForm(false)}>Cancel</button>
+      </div>
+    );
+}
+
+
+// Placeholder functions for adding cast and crew members
+function addCastMember() {
+    // Your logic to add a new cast member field
+}
+
+function addCrewMember() {
+    // Your logic to add a new crew member field
+}
+
+
+
+  if (selectedMovie) {
+    // Displaying selected movie details
+    return (
+      <div className="text-gray-800 bg-white p-24">
+        <h1 className="text-4xl font-bold mb-5">{selectedMovie.projectName}</h1>
+        <div>
+          <Image
+            src={`/images/${selectedMovie.photoExtension}.png`}
+            alt={selectedMovie.projectName}
+            width={200}
+            height={200}
+          />
+        </div>
+        <h2 className="text-2xl font-bold mt-4">Cast & Crew</h2>
+        <ul>
+          {selectedMovie.castAndCrew.map(member => (
+            <li key={member.walletAddress}>
+              {member.name} - Points: {member.points} - Wallet: {member.walletAddress}
+            </li>
+          ))}
+        </ul>
+        <h2 className="text-2xl font-bold mt-4">Daily Residual Payments</h2>
+        <ul>
+          {selectedMovie.dailyResidualPayments.map(day => (
+            <li key={day.date}>
+              <strong>{day.date}</strong> - Total Payments: {calculateTotalResiduals([day])} ETH
+              <ul>
+                {day.payments.map(payment => (
+                  <li key={payment.name}>
+                    {payment.name} - {payment.amount} ETH
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+        <button onClick={() => setSelectedMovie(null)} className="mt-5 bg-gray-200 py-2 px-4 rounded hover:bg-gray-300">
+          Back
+        </button>
+      </div>
+    );
+  }
+  
+
+  return (
+    <main className="flex min-h-screen flex-col justify-between px-24 pb-24 text-gray-800 bg-white">
+      <Image
+        src={"/images/logo.png"}
+        alt="logo"
+        width={220}
+        height={100}
+        className="mb-16 mt-10"
+      />
+      <button onClick={() => setShowCreateMovieForm(true)} className="mb-5 bg-blue-500 py-2 px-4 text-white rounded hover:bg-blue-600">
+        Create New Project
+      </button>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {mockData.map((movie: Movie) => (
-          <div key={movie.projectName} className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 cursor-pointer">
-            <h2 className="mb-3 text-2xl font-semibold" style={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>
-              {movie.projectName}
-            </h2>
-            <p className="m-0 max-w-[30ch] text-sm opacity-50">
-              Total Residuals: ${calculateTotalResiduals(movie.dailyResidualPayments).toLocaleString()}
-            </p>
+          <div key={movie.projectName} className="rounded-lg border border-gray-300 p-4 hover:border-gray-400 hover:shadow-lg cursor-pointer" onClick={() => setSelectedMovie(movie)}>
+            <h2 className="text-2xl font-semibold mb-2">{movie.projectName}</h2>
             <Image
-              src={`/images/${movie.photoExtension}.png`} // Updated to .png
+              src={`/images/${movie.photoExtension}.png`}
               alt={movie.projectName}
               width={100}
               height={100}
+              className="rounded-xl mb-2"
             />
-            {/* ... other movie details ... */}
+            <p>Total Paid: {calculateTotalResiduals(movie.dailyResidualPayments).toLocaleString()} ETH</p>
+            {/* Add additional movie details here as needed */}
           </div>
         ))}
       </div>
     </main>
-  )
+  );  
 }
---
+++ src/components/CastCrewMember.tsx
@@ -0,0 +1,118 @@
+import React, { useState } from 'react';
+
+interface CastCrewMember {
+  name: string;
+  points: number;
+  walletAddress: string;
+}
+
+const projectTypes = ["Feature Film", "Animated Film", "Documentary", "Short Film"];
+const genres = ["Drama", "Comedy", "Horror", "Science Fiction", "Thriller", "Family"];
+const distributionMethods = ["Netflix", "Apple TV", "Youtube"];
+const primaryDistributionMethods = ["Theatrical Release", "Streaming", "TV Broadcast"];
+
+export default function CreateMovieForm() {
+  const [typeOfProject, setTypeOfProject] = useState("");
+  const [genre, setGenre] = useState("");
+  const [distribution, setDistribution] = useState<string[]>([]);
+  const [primaryDistributionMethod, setPrimaryDistributionMethod] = useState("");
+  const [budget, setBudget] = useState("");
+  const [cast, setCast] = useState<CastCrewMember[]>([{ name: '', points: 0, walletAddress: '' }]);
+  const [crew, setCrew] = useState<CastCrewMember[]>([{ name: '', points: 0, walletAddress: '' }]);
+
+  const handleDistributionChange = (method: string) => {
+    if (distribution.includes(method)) {
+      setDistribution(distribution.filter(item => item !== method));
+    } else {
+      setDistribution([...distribution, method]);
+    }
+  };
+
+  const handleAddMember = (type: 'cast' | 'crew') => {
+    const newMember = { name: '', points: 0, walletAddress: '' };
+    if (type === 'cast') {
+      setCast([...cast, newMember]);
+    } else {
+      setCrew([...crew, newMember]);
+    }
+  };
+
+  const handleRemoveMember = (index: number, type: 'cast' | 'crew') => {
+    if (type === 'cast') {
+      setCast(cast.filter((_, i) => i !== index));
+    } else {
+      setCrew(crew.filter((_, i) => i !== index));
+    }
+  };
+
+  const handleMemberChange = (index: number, type: 'cast' | 'crew', key: keyof CastCrewMember, value: string | number) => {
+    const updatedMembers = type === 'cast' ? [...cast] : [...crew];
+    updatedMembers[index][key] = value;
+    type === 'cast' ? setCast(updatedMembers) : setCrew(updatedMembers);
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Form submission logic
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <select value={typeOfProject} onChange={e => setTypeOfProject(e.target.value)}>
+        <option value="">Select Project Type</option>
+        {projectTypes.map(type => <option key={type} value={type}>{type}</option>)}
+      </select>
+
+      <select value={genre} onChange={e => setGenre(e.target.value)}>
+        <option value="">Select Genre</option>
+        {genres.map(genre => <option key={genre} value={genre}>{genre}</option>)}
+      </select>
+
+      {distributionMethods.map(method => (
+        <label key={method}>
+          <input
+            type="checkbox"
+            checked={distribution.includes(method)}
+            onChange={() => handleDistributionChange(method)}
+          />
+          {method}
+        </label>
+      ))}
+
+      <select value={primaryDistributionMethod} onChange={e => setPrimaryDistributionMethod(e.target.value)}>
+        <option value="">Select Primary Distribution Method</option>
+        {primaryDistributionMethods.map(method => <option key={method} value={method}>{method}</option>)}
+      </select>
+
+      <input type="number" value={budget} onChange={e => setBudget(e.target.value)} placeholder="Budget" />
+
+      <div>
+        <h3>Cast</h3>
+        {cast.map((member, index) => (
+          <div key={index}>
+            <input type="text" value={member.name} onChange={e => handleMemberChange(index, 'cast', 'name', e.target.value)} placeholder="Name" />
+            <input type="number" value={member.points} onChange={e => handleMemberChange(index, 'cast', 'points', Number(e.target.value))} placeholder="Points" />
+            <input type="text" value={member.walletAddress} onChange={e => handleMemberChange(index, 'cast', 'walletAddress', e.target.value)} placeholder="Wallet Address" />
+            {cast.length > 1 && <button type="button" onClick={() => handleRemoveMember(index, 'cast')}>Remove</button>}
+          </div>
+        ))}
+        <button type="button" onClick={() => handleAddMember('cast')}>Add Cast Member</button>
+      </div>
+
+      <div>
+        <h3>Crew</h3>
+        {crew.map((member, index) => (
+          <div key={index}>
+            <input type="text" value={member.name} onChange={e => handleMemberChange(index, 'crew', 'name', e.target.value)} placeholder="Name" />
+            <input type="number" value={member.points} onChange={e => handleMemberChange(index, 'crew', 'points', Number(e.target.value))} placeholder="Points" />
+            <input type="text" value={member.walletAddress} onChange={e => handleMemberChange(index, 'crew', 'walletAddress', e.target.value)} placeholder="Wallet Address" />
+            {crew.length > 1 && <button type="button" onClick={() => handleRemoveMember(index, 'crew')}>Remove</button>}
+          </div>
+        ))}
+        <button type="button" onClick={() => handleAddMember('crew')}>Add Crew Member</button>
+      </div>
+
+      <button type="submit">Create Movie</button>
+    </form>
+  );
+}
